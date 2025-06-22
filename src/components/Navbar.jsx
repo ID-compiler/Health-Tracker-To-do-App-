812ab9b243a6fac65c6752950791fd9f6f0c4cb3
Ichha Dwivedi <ichhadwivedi2004@gmail.com>
2025-06-22 10:43:42 +0000

    Final task

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 const Navbar = () => {
   return (
-    <div className="bg-puple-700 text-white px-7 py-2">
+    <div className="bg-purple-700 text-white px-7 py-2">
       <ul className="flex flex-row justify-between items-center">
         <li className="font-bold text-lg">Health-tracker</li>
         <li>
           <ul className="flex flex-row space-x-8">
-            <li>Home</li>
-            <li>Your Plans</li>
+            <li className="hover:scale-110 transform transition-transform duration-300 cursor-pointer">
+              Home
+            </li>
+            <li className="hover:scale-110 transform transition-transform duration-300 cursor-pointer">
+              Your Plans
+            </li>
           </ul>
         </li>
       </ul>
